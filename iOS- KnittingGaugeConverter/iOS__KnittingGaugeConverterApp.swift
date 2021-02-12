d61319da93624f896e1832f024992e54a3802fb2
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-12 12:11:37 -0500

    Added iOS target using SwiftUI framework to project

---
+++ iOS- KnittingGaugeConverter/iOS__KnittingGaugeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS__KnittingGaugeConverterApp.swift
+//  iOS- KnittingGaugeConverter
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS__KnittingGaugeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
